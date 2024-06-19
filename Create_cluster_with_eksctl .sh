7702e27ff8a1fc9d382f9ed3d4b077cf22820b8c
vnicolas16 <61176533+vnicolas16@users.noreply.github.com>
2024-06-19 16:13:53 -0600

    Update Create_cluster_with_eksctl .sh

--- Create_cluster_with_eksctl .sh
+++ Create_cluster_with_eksctl .sh
@@ -1,1 +1,22 @@
-eksctl create cluster \ --name eks-mundos-e \ --region us-east-1 \ --node-type t3.small \ –nodes 3 \ --with-oidc \ --ssh-access \ --ssh-public-key pin \ --managed \ --full-ecr-access \ --zones us-east-1a,us-east-1b,us-east-1c 
+#!/bin/bash
+
+# Nombre del clúster EKS
+CLUSTER_NAME="eks-mundos-e"
+REGION="us-east-1"
+NODE_TYPE="t3.small"
+NODE_COUNT=3
+SSH_PUBLIC_KEY="pin"
+ZONES="us-east-1a,us-east-1b,us-east-1c"
+
+# Crear el clúster EKS
+eksctl create cluster \
+    --name "$CLUSTER_NAME" \
+    --region "$REGION" \
+    --node-type "$NODE_TYPE" \
+    --nodes "$NODE_COUNT" \
+    --with-oidc \
+    --ssh-access \
+    --ssh-public-key "$SSH_PUBLIC_KEY" \
+    --managed \
+    --full-ecr-access \
+    --zones "$ZONES"
