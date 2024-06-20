3bba095c8fac7533e210599a7b4e5c23d085766d
vnicolas16 <61176533+vnicolas16@users.noreply.github.com>
2024-06-20 17:13:52 -0600

    Create check_version.sh

---
+++ check_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check AWS CLI version
+aws --version
+
+# Check kubectl version
+kubectl version --client
+
+# Check Docker version
+docker version
+
+# Check Helm version
+helm version
+
+# Check Terraform version
+terraform version
