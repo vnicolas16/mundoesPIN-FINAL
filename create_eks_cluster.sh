5aeacf68557496ffe1fddb97293896415bad1e66
vnicolas16 <61176533+vnicolas16@users.noreply.github.com>
2024-06-28 11:05:28 -0600

    Update create_eks_cluster.sh

--- create_eks_cluster.sh
+++ create_eks_cluster.sh
@@ -14,3 +14,26 @@ eksctl create cluster \
 
 # Configurar kubectl para usar el contexto correcto
 aws eks --region us-east-1 update-kubeconfig --name eks-mundos-e
+
+# Crear y aplicar el aws-auth ConfigMap
+cat <<EOF > aws-auth-cm.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: arn:aws:iam::<YOUR_ACCOUNT_ID>:role/<YOUR_NODE_INSTANCE_ROLE>
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+  mapUsers: |
+    - userarn: arn:aws:iam::<YOUR_ACCOUNT_ID>:user/<YOUR_USER>
+      username: admin
+      groups:
+        - system:masters
+EOF
+
+kubectl apply -f aws-auth-cm.yaml
