5318806ddf85885bc707db4e4dcb586d50d3111c
vnicolas16 <61176533+vnicolas16@users.noreply.github.com>
2024-06-28 11:06:18 -0600

    Update create_eks_cluster.sh

--- create_eks_cluster.sh
+++ create_eks_cluster.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+# Actualizar el sistema
+sudo apt-get update -y
+
+# Instalar AWS CLI
+sudo apt-get install -y awscli
+
+# Configurar región por defecto
+export AWS_REGION="us-east-1"
+export AWS_DEFAULT_REGION="us-east-1"
+
+# Instalar kubectl
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Instalar eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin/eksctl
+
+# Crear el clúster EKS
 eksctl create cluster \
---name eks-mundos-e \
---region us-east-1 \
---node-type t3.small \
---nodes 3 \
---with-oidc \
---ssh-access \
---ssh-public-key pin \
---managed \
---full-ecr-access \
---zones us-east-1a,us-east-1b,us-east-1c
+  --name eks-mundos-e \
+  --region us-east-1 \
+  --node-type t3.small \
+  --nodes 3 \
+  --with-oidc \
+  --ssh-access \
+  --ssh-public-key pin \
+  --managed \
+  --zones us-east-1a,us-east-1b,us-east-1c
 
 # Configurar kubectl para usar el contexto correcto
 aws eks --region us-east-1 update-kubeconfig --name eks-mundos-e
